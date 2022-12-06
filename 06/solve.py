85712f1aa00f6af9c64cdb6a023829cd7eb899fb
Philip <44866668+px-d@users.noreply.github.com>
2022-12-06 17:45:57 +0100

    changed solve.py

--- 06/solve.py
+++ 06/solve.py
@@ -1,7 +1,7 @@
 from itertools import islice
 
 
-def signal_length(input, length):
+def frame(input, length):
     for i in range(len(input)):
         if (len(set(input[i:i + length])) == length):
             return i + length
@@ -9,4 +9,4 @@ def signal_length(input, length):
 
 with open("06/input.txt") as f:
     line = f.readline()
-    print(f"1: {signal_length(line, 4)}, 2: {signal_length(line, 14)}")
+    print(f"1: {frame(line, 4)}, 2: {frame(line, 14)}")
